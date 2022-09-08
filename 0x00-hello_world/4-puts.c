a2f71d048f8791b5099e045f875ab0d9923d7bf6
Jessica903 <sjmakhanya01@gmail.com>
2022-09-08 13:39:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
 * Returns: 0 after printing the function
 */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	returns (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+returns (0);
 }
