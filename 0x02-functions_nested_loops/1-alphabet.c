5bfa33c64587ff709d09558a657b0225cd9c5df3
Jessica903 <sjmakhanya01@gmail.com>
2022-09-13 09:49:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0 
  */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char c;
 
 	for (c ='a'; c <='z'; c++);
-	{		
+{		
 		_putchar(c);
-	}	
+}	
 
 _putchar('\n');
 }
