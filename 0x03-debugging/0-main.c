7ceab2b40aca6373e2ccb7ec1cc124e2196edf8a
Jessica903 <sjmakhanya01@gmail.com>
2022-09-15 10:18:15 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- *
  * main - prints positive or negative integer
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
-	i=0;
+	i = 0;
 
 	postitive_or_negative(i);
 
-	retun 0;
+	return 0;
 }
