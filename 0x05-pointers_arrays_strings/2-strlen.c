6d40765d57a8b05aeab4b0c04ba187b4e7721a02
Jessica903 <sjmakhanya01@gmail.com>
2022-09-19 13:52:58 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,16 +4,14 @@
  * _strlen - returns the leght of a string
  * @s: string
  * Return: returns the lenght as an integer
- * ;
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while(*(s + len) !='\O')
+	while(*(s + len) !='\0')
 			len++;
 
 	return (len);
 }	
-i
