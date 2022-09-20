bb9b511adf60c1cbdb223448fa2e07b1f5010053
Jessica903 <sjmakhanya01@gmail.com>
2022-09-20 12:06:30 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@ void puts_half(char *str)
 	int index, half;
 
 	index = 0;
-	while (str(index) != '\0')
+	while (*str(index) != '\0')
 			index++;
 
 	half = index / 2;
@@ -20,7 +20,7 @@ void puts_half(char *str)
 
 	while (half < index)
 	{
-		_putchar(str (half));
+		_putchar(*str (half));
 			half++;
 	}
 	_putchar('\n');
