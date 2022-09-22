9f6e90aefb45a4aa5eb2021b58dac6b5284ecc18
Jessica903 <sjmakhanya01@gmail.com>
2022-09-22 11:57:02 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,20 +2,22 @@
 #define MAIN_H
 
 /**
-* void prototypes()
-* int prototypes()
-*/
+ * Header file containing prototypes used for pointers, arrays and strings
+ */
 
+#include <stdlib.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int strlen_(char *s);
-void puts_char(char *str);
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *scr);
-int _atoi(char *str);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
-#endif /*MAIN_H*/
+#endif
